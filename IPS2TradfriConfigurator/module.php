f488bf9cc510e258d9539e1a0c44dccbf1735ed9
Joey-1970 <paeper@horburg.de>
2020-02-22 19:23:15 +0100

    Update module.php

--- IPS2TradfriConfigurator/module.php
+++ IPS2TradfriConfigurator/module.php
@@ -9,6 +9,7 @@
             	// Diese Zeile nicht löschen.
             	parent::Create();
 		$this->ConnectParent("{562389F8-739F-644A-4FC7-36F2CE3AFE4F}");
+		$this->RegisterPropertyInteger("Category", 0);  
 		
         }
  	
