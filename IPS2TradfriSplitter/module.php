14566029225c2fa9229f5ddde2163c409616bb61
Joey-1970 <paeper@horburg.de>
2020-02-27 21:46:20 +0100

    Update module.php

--- IPS2TradfriSplitter/module.php
+++ IPS2TradfriSplitter/module.php
@@ -138,8 +138,7 @@
 			$IP = $this->ReadPropertyString("GatewayIP");
 			$Key = $this->ReadAttributeString("PresharedKey");
 			$Identifier = $this->ReadAttributeString("Identifier");
-			$AmmbienteArray = array(0 => "f1e0b5", 1 => "f5faf6", 2 => "efd275");
-			$Message = 'sudo coap-client -m put -u "'.$Identifier.'" -k "'.$Key.'" -e \'{ "3311": [{ "5706": "'.$AmmbienteArray[$Value].'" }] }\' "coaps://'.$IP.':5684/15001/'.$DeviceID.'"'; 
+			$Message = 'sudo coap-client -m put -u "'.$Identifier.'" -k "'.$Key.'" -e \'{ "3311": [{ "5706": "'.$Value.'" }] }\' "coaps://'.$IP.':5684/15001/'.$DeviceID.'"'; 
 			$Response = exec($Message." 2>&1", $Output);
 			$this->SendDebug("BulbAmbiente", "Ergebnis: ".serialize($Output), 0);
 		}
