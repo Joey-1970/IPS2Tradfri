d42052fb48fd150d8bfc415cd5ec9f7700ff6a8f
Joey-1970 <paeper@horburg.de>
2020-07-20 22:46:24 +0200

    Update module.php

--- IPS2TradfriPlug/module.php
+++ IPS2TradfriPlug/module.php
@@ -83,7 +83,7 @@
 	public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
     	{
 		switch ($Message) {
-			case 10001:
+			case 10100:
 				// IPS_KERNELSTARTED
 				$this->ApplyChanges();
 				break;
