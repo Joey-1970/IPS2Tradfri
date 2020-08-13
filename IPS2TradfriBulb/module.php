402f8405751cf4a3df232a73b1ab9d1fc9252a2a
Joey-1970 <paeper@horburg.de>
2020-08-13 15:38:18 +0200

    Update module.php

--- IPS2TradfriBulb/module.php
+++ IPS2TradfriBulb/module.php
@@ -69,7 +69,7 @@
 		
 		$this->RegisterVariableInteger("Color", "Farbe", "Tradfri.Color", 60);
 	       
-		//$this->RegisterVariableInteger("RGB", "Farbe", "~HexColor", 70);
+		$this->RegisterVariableInteger("RGB", "Farbe", "~HexColor", 70);
 		
 		$this->RegisterVariableBoolean("Available", "Verfügbar", "~Alert.Reversed", 80);
         }
@@ -196,16 +196,16 @@
 					SetValueInteger($this->GetIDForIdent($Ident), $Value);
 					$this->GetState();
 				break;
-				/*
+				
 				case "RGB":
 					// Wert von RGB in xyY wandeln
 					$CIE = $this->HexToCIE($Value);
 					$Result = $this->SendDataToParent(json_encode(Array("DataID"=> "{4AA318CB-CA9A-2467-3079-A35AD1577771}", 
-						"Function" => "BulbRGB", "DeviceID" => $this->ReadPropertyInteger("DeviceID"), "ValueX" => intval($CIE['x'] * 1000), "ValueY" => intval($CIE['y'] * 1000) )));
+						"Function" => "BulbRGB", "DeviceID" => $this->ReadPropertyInteger("DeviceID"), "ValueX" => $CIE['x'], "ValueY" => $CIE['y'] )));
 					SetValueInteger($this->GetIDForIdent($Ident), $Value);
 					$this->GetState();
 				break;
-				*/
+				
 				default:
 				    throw new Exception("Invalid Ident");
 		    	}
@@ -300,7 +300,84 @@
 			}
 		}
 	}    
+	
+	// Kais Funktionen
+	private function HexToCIE($HexColor)
+	{
+		$red = (($HexColor >> 16) & 0xFF);
+		$green = (($HexColor >> 8) & 0xFF);
+		$blue = ($HexColor & 0xFF);
+		
+		$red = ($red > 0.04045) ? pow(($red + 0.055) / (1.0 + 0.055), 2.4) : ($red / 12.92);
+		$green = ($green > 0.04045) ? pow(($green + 0.055) / (1.0 + 0.055), 2.4) : ($green / 12.92);
+		$blue = ($blue > 0.04045) ? pow(($blue + 0.055) / (1.0 + 0.055), 2.4) : ($blue / 12.92);
+
+		$X = $red * 0.664511 + $green * 0.154324 + $blue * 0.162028;
+		$Y = $red * 0.283881 + $green * 0.668433 + $blue * 0.047685;
+		$Z = $red * 0.000088 + $green * 0.072310 + $blue * 0.986039;
+		//$this->SendDebug('RGBToCIE', 'X: ' . $X . ' Y: ' . $Y . ' Z: ' . $Z, 0);
+
+		
+		$cie['x'] = round(($X / ($X + $Y + $Z) * 65536), 0);
+		$cie['y'] = round(($Y / ($X + $Y + $Z) * 65536), 0);
+		
+	return $cie;
+	} 
 	 
+	private function CIEToRGB($x, $y, $brightness = 255, $opt = false)
+	{
+
+		if ($opt) {
+		    $x = $x / 65536;
+		    $y = $y / 65536;
+		}
+
+		$z = 1.0 - $x - $y;
+		$Y = $brightness / 255;
+		$X = ($Y / $y) * $x;
+		$Z = ($Y / $y) * $z;
+
+		$red = $X * 1.656492 - $Y * 0.354851 - $Z * 0.255038;
+		$green = -$X * 0.707196 + $Y * 1.655397 + $Z * 0.036152;
+		$blue = $X * 0.051713 - $Y * 0.121364 + $Z * 1.011530;
+
+		if ($red > $blue && $red > $green && $red > 1.0) {
+		    $green = $green / $red;
+		    $blue = $blue / $red;
+		    $red = 1.0;
+		} elseif ($green > $blue && $green > $red && $green > 1.0) {
+		    $red = $red / $green;
+		    $blue = $blue / $green;
+		    $green = 1.0;
+		} elseif ($blue > $red && $blue > $green && $blue > 1.0) {
+		    $red = $red / $blue;
+		    $green = $green / $blue;
+		    $blue = 1.0;
+		}
+		$red = $red <= 0.0031308 ? 12.92 * $red : (1.0 + 0.055) * $red ** (1.0 / 2.4) - 0.055;
+		$green = $green <= 0.0031308 ? 12.92 * $green : (1.0 + 0.055) * $green ** (1.0 / 2.4) - 0.055;
+		$blue = $blue <= 0.0031308 ? 12.92 * $blue : (1.0 + 0.055) * $blue ** (1.0 / 2.4) - 0.055;
+
+		$red = ceil($red * 255);
+		$green = ceil($green * 255);
+		$blue = ceil($blue * 255);
+
+		$color = sprintf('#%02x%02x%02x', $red, $green, $blue);
+
+	return $color;
+	} 
+	    
+	private function HexToRGB($value)
+	{
+		$RGB = [];
+		$RGB[0] = (($value >> 16) & 0xFF);
+		$RGB[1] = (($value >> 8) & 0xFF);
+		$RGB[2] = ($value & 0xFF);
+		//$this->SendDebug('HexToRGB', 'R: ' . $RGB[0] . ' G: ' . $RGB[1] . ' B: ' . $RGB[2], 0);
+	return $RGB;
+	} 
+	
+	/*
 	private function HexToCIE($Hex)
     	{
         	$this->SendDebug("HexToCIE", "Wert: ".$Hex, 0);
@@ -358,6 +435,7 @@
 		$Hex = hexdec(str_pad(dechex($red), 2,'0', STR_PAD_LEFT).str_pad(dechex($green), 2,'0', STR_PAD_LEFT).str_pad(dechex($blue), 2,'0', STR_PAD_LEFT));
 	return $Hex;
     	}    
+	*/
 	    
 	private function RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize)
 	{
