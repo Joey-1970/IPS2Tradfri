0d19601962f116fe3ab09b4227fcdbaa3e241e00
Joey-1970 <paeper@horburg.de>
2020-08-14 13:21:12 +0200

    Update module.php

--- IPS2TradfriBulb/module.php
+++ IPS2TradfriBulb/module.php
@@ -364,10 +364,15 @@
 		$red = ceil($red * 255);
 		$green = ceil($green * 255);
 		$blue = ceil($blue * 255);
-
+		
+		$red = (($HexColor >> 16) & 0xFF);
+		$green = (($HexColor >> 8) & 0xFF);
+		$blue = ($HexColor & 0xFF);
+		
+		$HexAlt = ($red << 16) & ($green << 8) & $blue; 
 		//$color = sprintf('#%02x%02x%02x', $red, $green, $blue);
 		$Hex = hexdec(str_pad(dechex($red), 2,'0', STR_PAD_LEFT).str_pad(dechex($green), 2,'0', STR_PAD_LEFT).str_pad(dechex($blue), 2,'0', STR_PAD_LEFT));
-
+		$this->SendDebug('CIEToRGB', 'HexAlt: '.$HexAlt.' Hex: '.$Hex, 0);
 	return $Hex;
 	} 
 	    
