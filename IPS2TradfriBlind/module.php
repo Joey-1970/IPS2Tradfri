4b06c4ca6b236f0b4a93055ae92488eec48be0c5
Joey-1970 <paeper@horburg.de>
2021-12-13 15:56:24 +0100

    Update module.php

--- IPS2TradfriBlind/module.php
+++ IPS2TradfriBlind/module.php
@@ -86,7 +86,7 @@
 	public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
     	{
 		switch ($Message) {
-			case 10001:
+			case IPS_KERNELSTARTED:
 				// IPS_KERNELSTARTED
 				$this->ApplyChanges();
 				break;
