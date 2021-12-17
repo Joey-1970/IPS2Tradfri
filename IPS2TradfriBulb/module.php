2967e5db317c735c41c1c030cb269bb3c0f0138e
Joey-1970 <paeper@horburg.de>
2021-12-17 20:32:44 +0100

    Update module.php

--- IPS2TradfriBulb/module.php
+++ IPS2TradfriBulb/module.php
@@ -120,7 +120,9 @@
 					IPS_SetHidden($this->GetIDForIdent("RGB"), true); 
 				}
 
-				$this->SetStatus(102);
+				If ($this->GetStatus() <> 102) {
+					$this->SetStatus(102);
+				}
 				If (IPS_GetKernelRunlevel() == KR_READY) {
 					$this->GetDeviceInfo();
 					$this->GetState();
@@ -130,11 +132,15 @@
 			else {
 				Echo "Syntax der Device ID inkorrekt!";
 				$this->SendDebug("ApplyChanges", "Syntax der Device ID inkorrekt!", 0);
-				$this->SetStatus(203);
+				If ($this->GetStatus() <> 202) {
+					$this->SetStatus(202);
+				}
 			}
 		}
 		else {
-			$this->SetStatus(104);
+			If ($this->GetStatus() <> 104) {
+				$this->SetStatus(104);
+			}
 			$this->SetTimerInterval("Timer_1", 0);
 		}	
 	}
